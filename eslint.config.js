33b8e0008f1e473dc6692d4fabe15efb0f7b122a
i02sdarling <2668933894@qq.com>
2024-11-05 18:30:29 +0800

    fix:remove line error

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import js from '@eslint/js'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default antfu({
   extends: [js.configs.recommended, ...tseslint.configs.recommended],
