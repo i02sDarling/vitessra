1ff3a0f6e6d56b775546865870b43f1347482b4e
i02sdarling <2668933894@qq.com>
2024-11-05 14:28:24 +0800

    chore:install unocss & preset

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 text="red" flex='~ 1' >Vite + React</h1>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button text="red lg" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
